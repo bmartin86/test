d3040976bfa21936891114970dfc219edadcde7d
bmartin86 <iMuy11pMpk>
2021-10-19 16:28:25 +0200

    admin.index

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -3,6 +3,7 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
           <b>{{ __('All users') }}</b>
         </h2>
+        <div class="medium-2  columns"><a class="button hollow success" href="">ADD NEW USER</a></div>
     </x-slot>
 {{-- {{dd($data)}} --}}
 <div class="py-12">
@@ -15,6 +16,7 @@
                     <th scope="col">Username</th>
                     <th scope="col">Email</th>
                     <th scope="col">Role</th>
+                    <th scope="col">Action</th>
                   </tr>
                 </thead>
                 <tbody>
@@ -24,6 +26,8 @@
                     <td>{{ $user->username }}</td>
                     <td>{{ $user->email }}</td>
                     <td>{{ $user->role }}</td>
+                    <td><a class="hollow button warning" href="">EDIT</a>
+                    <a class="hollow button warning" href="">DELETE</a></td>
                   </tr>
                 @endforeach
                 </tbody>
